986c651e063e84b835938c7afb86df0f87aeab24
Kaspar Sinitsin <kaspar.sinitsin@students.beaminstitute.org>
2025-02-05 20:09:50 +0100

    Restructured files for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import Form from "./components/Form"
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { useState } from 'react';
+import Home from './pages/Home';
+import Calendar from './pages/Calendar';
+import Entry from './pages/Entry';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);  
+  };
 
   return (
-    <div>
-        <Form />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path='/' 
+          element={<MainLayout cart={cart} addToCart={addToCart} />} 
+        >
+          <Route index element={<Home />} />
+          <Route path='calendar' element={<Calendar />} />
+          <Route path='entry/:entry' element={<Entry />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

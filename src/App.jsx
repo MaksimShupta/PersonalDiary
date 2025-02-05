aba29610369727ea645c08f13aa590a385d90bf6
Kaspar Sinitsin <kaspar.sinitsin@students.beaminstitute.org>
2025-02-05 20:36:10 +0100

    Fixed some issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,18 @@ import Calendar from './pages/Calendar';
 import Entry from './pages/Entry';
 
 function App() {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => {
-    setCart([...cart, product]);  
-  };
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route 
-          path='/' 
-          element={<MainLayout cart={cart} addToCart={addToCart} />} 
-        >
-          <Route index element={<Home />} />
+        <Route >
+          <Route path='/' element={<Home />} />
           <Route path='calendar' element={<Calendar />} />
           <Route path='entry/:entry' element={<Entry />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
